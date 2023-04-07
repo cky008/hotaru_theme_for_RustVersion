0b7990756533ab40f657937ccaa9cf6bd97bc0b7
Kaiyu Chen <56156916+cky008@users.noreply.github.com>
2023-04-07 16:33:20 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,5 +2,5 @@ window.__PRE_CONFIG__ = {
   header: 'Server Status',
   subHeader: 'Servers\' Probes Set up with ServerStatus',
   interval: 1.5,
-  footer: '© 2023 <a href="https://iocky.com/">星笙月煜</a>. <p>Powered by <a href="https://github.com/zdz/ServerStatus-Rust">ServerStatus-Rust</a> and <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p> '
+  footer: '<p>© 2023 <a href="https://iocky.com/">星笙月煜</a>. Powered by <a href="https://github.com/zdz/ServerStatus-Rust">ServerStatus-Rust</a> and <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p> '
 };
