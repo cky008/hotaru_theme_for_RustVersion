73634efc82a9f08e308dcd1eac11b69523ea2184
Kaiyu Chen <greatcky83@gmail.com>
2023-04-07 16:26:04 +0100

    config footer

--- public/config.js
+++ public/config.js
@@ -2,5 +2,5 @@ window.__PRE_CONFIG__ = {
   header: 'Server Status',
   subHeader: 'Servers\' Probes Set up with ServerStatus',
   interval: 1.5,
-  footer: '<p>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p>'
+  footer: '© 2023 <a href="https://iocky.com/">星笙月煜</a>. <p>Powered by <a href="https://github.com/zdz/ServerStatus-Rust">ServerStatus-Rust</a> and <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p> '
 };
